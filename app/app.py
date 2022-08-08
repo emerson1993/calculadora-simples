aa413f388f426a9b22c8c911343c8d4d8630d730
Emerson F. Oliveira <99295164+emerson1993@users.noreply.github.com>
2022-08-07 21:26:17 -0300

    Adicionando comentários ao código

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
+"""
+Utilizamos neste código a biblioteca tkinter.
+"""
 from tkinter import *
-from tkinter import ttk
-from PIL import ImageTk,Image
 from tkinter import messagebox
-import time
 
-### FUNCOES:
 def somar():
+    """
+    A função realiza operações de adição, e retorna o resultado na tela.
+    """
     numero1 = float(caixa_editavel_1.get())
     numero2 = float(caixa_editavel_2.get())
 
@@ -18,6 +20,9 @@ def somar():
     text.place(x=150, y=150)
     
 def subtrair():
+    """
+    A função realiza operações de subtração, e retorna o resultado na tela.
+    """
     numero1 = float(caixa_editavel_1.get())
     numero2 = float(caixa_editavel_2.get())
 
@@ -30,6 +35,10 @@ def subtrair():
     text.place(x=150, y=150)
     
 def multiplicar():
+    """
+    Função realiza operação de multiplicação, e retorna o resultado na tela.
+    """
+
     numero1 = float(caixa_editavel_1.get())
     numero2 = float(caixa_editavel_2.get())
 
@@ -42,6 +51,10 @@ def multiplicar():
     text.place(x=150, y=150)
     
 def dividir():
+    """
+    Função realiza operação de divisão, e retorna o resultado na tela.
+    """
+
     numero1 = float(caixa_editavel_1.get())
     numero2 = float(caixa_editavel_2.get())
 
@@ -54,6 +67,10 @@ def dividir():
     text.place(x=150, y=150)
     
 def porcentagem():
+    """
+    Função realiza operação de porcentagem, e retorna o resultado na tela.
+    """
+
     numero1 = float(caixa_editavel_1.get())
     numero2 = float(caixa_editavel_2.get())
 
@@ -67,28 +84,40 @@ def porcentagem():
     text.place(x=150, y=150)
     
 def finalizar():
+    """
+    Função mostra uma mensagem na tela, e tem função de destruir a janela.
+    """
     if messagebox.askokcancel("Encerrando...", "Tem certeza que deseja sair?"):
         interface_1.destroy()
 
 def sair(event):
+    """
+    Chama a função finalizar.
+    """
     finalizar()
 
-#### JANELA PRINCIPAL:
+"""
+A seguir, primeiro criamos a janela interface_1 e adicionamos os dados das informações da janela
+a dimensão, icone, título e cor.
+"""
 interface_1 = Tk()
-menubar = Menu(interface_1)
 interface_1.geometry("444x350")
 interface_1.resizable(0,0)
 interface_1.title("Calculadora")
 interface_1.configure(background='#0C0C0C')
 
-#### LABEL
+"""
+Textos que são exibidos ao criar a janela.
+"""
 text = Label(interface_1, text="C A L C U L A D O R A", bg="#0C0C0C", font=("Arial", 20, "bold"), fg="#FFFFFF", cursor="hand2")
 text.place(x=70, y=5)
 
 text = Label(interface_1, text="_______________________", bg="#0C0C0C", font=("Arial", 20, "bold"), fg="#FFFFFF", cursor="hand2")
 text.place(x=50, y=90)
 
-#### CAIXAS EDITÁVEIS
+"""
+Caixas editáveis para que os dados do usuário sejam adicionados.
+"""
 caixa_editavel_1 = StringVar()
 caixa_editavel_1Box = Entry(interface_1, font=("Arial", 30, "bold"), textvariable=caixa_editavel_1)
 caixa_editavel_1Box.place(x= 65, y=55, width=100, height=50)
@@ -97,7 +126,10 @@ caixa_editavel_2 = StringVar()
 caixa_editavel_2Box = Entry(interface_1, font=("Arial", 30, "bold"), textvariable=caixa_editavel_2)
 caixa_editavel_2Box.place(x= 280, y=55, width=100, height=50)
 
-#### BOTÕES
+"""
+Os botões a seguir são responsáveis pelas operações matemáticas e estão vinculados
+as funções somar, subtrair, multiplicar, dividir, porcentagem e sair.
+"""
 botao_1 = Button(interface_1, text="+", font=("Arial", 20, "bold"), command=somar)
 botao_1.config(height=1, width=4)
 botao_1.place(x=10, y=250)
